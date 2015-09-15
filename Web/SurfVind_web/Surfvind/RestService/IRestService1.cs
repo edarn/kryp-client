b7c84571c64781132aeb2d50bd623bc034ce7aac
Thomas Hermansson <thomas@tna.se>
2015-09-15 20:44:52 +0200

    Just an example RestService added

---
+++ Web/SurfVind_web/Surfvind/RestService/IRestService1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+using System.IO;
+
+namespace Surfvind_2011
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRestService1" in both code and config file together.
+    [ServiceContract]
+    public interface IRestService1
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+                    ResponseFormat = WebMessageFormat.Json,
+                    UriTemplate = "{imei}/json")]
+        string JsonData(string imei);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+                    ResponseFormat = WebMessageFormat.Json,
+                    RequestFormat = WebMessageFormat.Json,
+                    UriTemplate = "{imei}/send")]
+        void SendImage(RequestData request, string imei);
+
+
+    }
+    [DataContract]
+    public class RequestData
+    {
+        [DataMember]
+        String First { get; set; }
+        [DataMember]
+        String Second { get; set; }
+    }
+}
+
---
+++ Web/SurfVind_web/Surfvind/RestService/RestService1.svc.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.IO;
+using System.ServiceModel.Web;
+using System.Drawing;
+
+namespace Surfvind_2011
+
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RestService1" in code, svc and config file together.
+    public class RestService1 : IRestService1
+    {
+        public string JsonData(string imei)
+        {
+            return "Dicken " + imei;
+        }
+        public Stream GetImage()
+        {
+            var m = new MemoryStream();
+            //Fill m
+
+            Bitmap bmp = new Bitmap(50, 50);
+            Graphics g = Graphics.FromImage(bmp);
+            g.FillRectangle(Brushes.Green, 0, 0, 50, 50);
+            g.Dispose();
+
+            
+            bmp.Save(m, System.Drawing.Imaging.ImageFormat.Png);
+            bmp.Dispose();
+            
+
+
+            // very important!!! otherwise the client will receive content-length:0
+            m.Position = 0;
+
+            WebOperationContext.Current.OutgoingResponse.ContentType = "image/png";
+            WebOperationContext.Current.OutgoingResponse.ContentLength = m.Length;
+            return m;
+        }
+
+        public void SendImage(RequestData request, string imei)
+        {
+            Console.Out.WriteLine(request);
+
+            /*
+            image.Position = 0;
+            Bitmap bmp = new Bitmap(image);
+
+            bmp.Save(System.Web.HttpContext.Current.Server.MapPath("~/Images/" + imei + "_ScreenShot.png"), System.Drawing.Imaging.ImageFormat.Png);
+            bmp.Dispose();
+            */
+        }
+    }
+}
